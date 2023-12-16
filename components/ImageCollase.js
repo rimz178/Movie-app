64f3628617d1f849b9cfb82af2644c5dc4c573eb
riku <riku.mikkonen@tuni.fi>
2023-12-16 12:14:21 +0200

    modify imagecollase file, add pagination component

--- components/ImageCollase.js
+++ components/ImageCollase.js
@@ -1,7 +1,7 @@
 import { View, StyleSheet, Dimensions, Text, Image } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import data from "../data";
-import Carousel from "react-native-snap-carousel";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 import Colors from "../Colors/Colors";
 
 export default function ImageCollase() {
@@ -9,8 +9,10 @@ export default function ImageCollase() {
 
   const sliderWidth = screenWidth;
   const itemWidth = screenWidth * 8;
+  const [activeDotIndex, setActiveDoIndex] = useState(0);
+  const _carousel = useRef();
 
-  const renderItem = ({ item }) => {
+  const renderItem = ({ item, index }) => {
     return (
       <View>
         <Image source={item.imgUrl} style={styles.itemImg} />
@@ -22,31 +24,31 @@ export default function ImageCollase() {
     <View>
       <Text style={styles.text}>Now Playing Cinemas</Text>
       <Carousel
-        layout="tinder"
+        ref={_carousel}
+        layout="default"
         data={data}
         renderItem={renderItem}
         firstItem={1}
         sliderWidth={sliderWidth}
         itemWidth={itemWidth}
+        onSnapToItem={(index) => setActiveDoIndex(index)}
       />
+      <View>
+        <Pagination
+          carouselRef={_carousel}
+          activeDotIndex={activeDotIndex}
+          dotsLength={3}
+        />
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  itemContainer: {
-    marginTop: 20,
-    backgroundColor: Colors.white,
-    borderRadius: 8,
-    padding: 20,
-    justifyContent: "center",
-    height: 450,
-    borderRadius: 20,
-  },
   itemImg: {
-    margin: 20,
+    margin: 15,
     padding: 20,
-    width: 250,
-    height: 250,
+    width: 350,
+    height: 350,
     borderRadius: 20,
     alignItems: "center",
   },
