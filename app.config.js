c5e15be470efe1b6fb865889ce79fc8ab11000a0
Riku <riku.mikkonen@tuni.fi>
2025-10-24 16:12:16 +0300

    chore: update dependencies in package.json
    - upgraded expo from 54.0.13 to 54.0.20
    - upgraded expo-constants from ~18.0.9 to ~18.0.10
    - upgraded expo-dev-client from ~6.0.15 to ~6.0.16
    - upgraded react-native from 0.81.4 to 0.81.5

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   name: "Movie-App",
   slug: "movie-app",
-  version: "1.4.0",
+  version: "1.5.0",
   orientation: "portrait",
   icon: "./assets/movieicon.png",
   jsEngine: "hermes",
@@ -47,7 +47,7 @@ export default {
     checkAutomatically: "ON_LOAD",
     fallbackToCacheTimeout: 5000,
   },
-  runtimeVersion: "1.4.0",
+  runtimeVersion: "1.5.0",
   plugins: [
     [
       "expo-notifications",
